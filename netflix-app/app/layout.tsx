b5730a3bc4c3eae468b9ad309d25bcd42000a91c
yeji <oyatpeach@naver.com>
2023-05-13 00:20:24 +0900

    style: head 변경

--- netflix-app/app/layout.tsx
+++ netflix-app/app/layout.tsx
@@ -7,6 +7,10 @@ export default function RooteLayout({
 }) {
   return (
     <html>
+      <head>
+        <title>Repick_Netflix</title>
+        <link rel='icon' href='/netflix_trans.png'/>
+        </head>
       <body className="w375 h746 ma"><StyledComponentsRegistry>{children}</StyledComponentsRegistry></body>
     </html>
   );
