593f8a90dda0c81bb763f6d11f08ed3067e7eb9c
yeji <oyatpeach@naver.com>
2023-05-17 17:14:40 +0900

    feat: datail page에 play, back button 생성

--- netflix-app/app/detail/[...slug]/page.tsx
+++ netflix-app/app/detail/[...slug]/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import { useState, useEffect } from 'react';
+import playButton from './../../../assets/components/images/Button/play.svg';
 
 const DetailPage = ({
   params,
@@ -34,12 +35,52 @@ const DetailPage = ({
         <BackImg
           src={`https://image.tmdb.org/t/p/original/${params.slug[1]}`}
         />
+        <PlayButton>
+          <ButtonImage src={playButton} />
+          <div className="text">{'Play'}</div>
+        </PlayButton>
         <Title>Previews</Title>
         <Preview>{params.slug[2]}</Preview>
+        <BackButton
+          onClick={() => {
+            router.push('/home');
+          }}
+        >
+          x
+        </BackButton>
       </Header>
     </>
   );
 };
+const BackButton = styled.button``;
+const ButtonImage = styled.img``;
+const PlayButton = styled.div`
+  height: 45px;
+  background: #c4c4c4;
+  border-radius: 5.625px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 19px;
+  margin: 13px 36px 0 36px;
+  cursor: pointer;
+
+  .text {
+    width: 39px;
+    height: 30px;
+    padding-left: 15px;
+    user-select: none;
+    cursor: pointer;
+
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20.4624px;
+    line-height: 30px;
+
+    color: #000000;
+  }
+`;
 const Title = styled.div`
   width: 111px;
   height: 20px;
