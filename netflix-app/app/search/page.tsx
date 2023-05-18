a683c896a70e0174be5de00375c2f2c9f728e3f2
yeji <oyatpeach@naver.com>
2023-05-19 00:46:03 +0900

    style: search input style 수정

--- netflix-app/app/search/page.tsx
+++ netflix-app/app/search/page.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Footer from '@/assets/components/Common/Footer';
 import SearchList from '@/assets/components/SearchList';
 import { fetchSearch, fetchTopRated } from '@/assets/api/requests';
-import {BiSearch, BiX} from 'react-icons/bi';
+import { BiSearch, BiX } from 'react-icons/bi';
 import SearchResultText from '@/assets/components/SearchResultText';
 
 async function getSearchData(e: string) {
@@ -19,8 +19,8 @@ async function getTopRatedData() {
 
 export default function SearchPage() {
   const [searched, setSearched] = useState([] as any);
-	const [inputText, setInputText] = useState("");
-	const [isSearching, setIsSearching] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
   useEffect(() => {
     async function fetchData() {
       const topRatedData = await getTopRatedData();
@@ -28,61 +28,59 @@ export default function SearchPage() {
     }
     fetchData();
   }, []);
-	useEffect(() => {
-		if(inputText.length == 0)
-			setIsSearching(false);
-		else
-			setIsSearching(true);
+  useEffect(() => {
+    if (inputText.length == 0) setIsSearching(false);
+    else setIsSearching(true);
   }, [inputText]);
-  const handleChange = async (e: { target: { value: any; }; }) => {
-		setInputText(e.target.value);
-    if(e.target.value === ''){
-			const initialResults = await getTopRatedData();
-			setSearched(initialResults[0]);
+  const handleChange = async (e: { target: { value: any } }) => {
+    setInputText(e.target.value);
+    if (e.target.value === '') {
+      const initialResults = await getTopRatedData();
+      setSearched(initialResults[0]);
+    } else {
+      const searchResults = await getSearchData(e.target.value);
+      setSearched(searchResults);
     }
-		else{
-			const searchResults = await getSearchData(e.target.value);
-			setSearched(searchResults);
-		}
   };
-	const handleDelete = () =>{
-		setInputText("");
-	}
+  const handleDelete = () => {
+    setInputText('');
+  };
   return (
     <>
-			<SearchWrapper>
-				<SearchInputWrapper>
-					<BiSearch color = "#FFFFFF" size={22} />
-					<SearchBox
-						value = {inputText}
-						onChange={handleChange}
-						placeholder="Search for a movie"
-					></SearchBox>
-					<XButtonWrapper>
-						<BiX color = "#FFFFFF" size={27} onClick = {handleDelete}/>
-					</XButtonWrapper>
-				</SearchInputWrapper>
-				
-				<SearchResultText isSearch = {isSearching}/>
-				
-				<SearchList movies={searched} />
-			</SearchWrapper>
+      <SearchWrapper>
+        <SearchInputWrapper>
+          <ButtonWrapper>
+            <BiSearch color="#FFFFFF" size={22} />
+          </ButtonWrapper>
+          <SearchBox
+            value={inputText}
+            onChange={handleChange}
+            placeholder="Search for a movie"
+          ></SearchBox>
+          <ButtonWrapper>
+            <BiX color="#FFFFFF" size={27} onClick={handleDelete} />
+          </ButtonWrapper>
+        </SearchInputWrapper>
+
+        <SearchResultText isSearch={isSearching} />
+
+        <SearchList movies={searched} />
+      </SearchWrapper>
       <Footer />
     </>
   );
 }
 const SearchWrapper = styled.div`
-	padding-bottom : 45px;
-`
+  padding-bottom: 45px;
+`;
 const SearchInputWrapper = styled.div`
-	display : flex;
-	align-items : center;
-	margin-top : 50px;
-	height : 50px;
-	
+  display: flex;
+  align-items: center;
+  margin-top: 50px;
+  height: 50px;
+
   background: #424242;
-	
-`
+`;
 const SearchBox = styled.input`
   width: 100%;
 
@@ -91,13 +89,15 @@ const SearchBox = styled.input`
   font-weight: 400;
   font-size: 15.213px;
   line-height: 31px;
-	border: none;
+  border: none;
 
   letter-spacing: 0.206667px;
 
   background: #424242;
+  outline: none;
 `;
 
-const XButtonWrapper = styled.div`
-	cursor : pointer;
-`
+const ButtonWrapper = styled.div`
+  cursor: pointer;
+  margin: 5px 12px 0 12px;
+`;
