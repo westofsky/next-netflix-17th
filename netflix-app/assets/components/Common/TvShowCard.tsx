9094630d85e1c3fb8acae691df01de521147e109
westofsky <westofsky159@gmail.com>
2023-05-19 21:23:03 +0900

    chore : 주석 제거

--- netflix-app/assets/components/Common/TvShowCard.tsx
+++ netflix-app/assets/components/Common/TvShowCard.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function TvShowCard({movieData} : any) {
-  console.log(movieData);
   return (
     <>
       <Movies>
